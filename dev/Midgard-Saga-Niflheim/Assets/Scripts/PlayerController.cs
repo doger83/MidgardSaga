50dd98b494ec0bce8d3df653faf7b3e0f97f9520
Dominic Gerweck <doger83@users.noreply.github.com>
2020-05-09 23:01:51 +0200

    fixed pixelSna bug

--- dev/Midgard-Saga-Niflheim/Assets/Scripts/PlayerController.cs
+++ dev/Midgard-Saga-Niflheim/Assets/Scripts/PlayerController.cs
@@ -20,26 +20,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // at the beginning the Player is NOT moving!
         playerMoving = false;
 
-        // move Player left or right
+        // Movement LEFT or RIGHT
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
+            // Move the PlayerSprite
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            
+            // Animate the Player Sprite while moving
             playerMoving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"),0f );
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
-        // move Player up or down
+        // Movement UP or DOWN
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
+            // Move the PlayerSprite
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+
+            // Animate the Player Sprite while moving
             playerMoving = true;
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
 
+        // get input for Movement-Animation
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
+        // Animate the Player if he is Moving
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.y);
