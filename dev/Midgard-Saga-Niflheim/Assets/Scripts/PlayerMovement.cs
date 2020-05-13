b6cff97e84b6595082292b4774113df67cc901c2
Dominic Gerweck <doger83@users.noreply.github.com>
2020-05-13 23:20:51 +0200

    add template sprites for basic isometric character animation

--- dev/Midgard-Saga-Niflheim/Assets/Scripts/PlayerMovement.cs
+++ dev/Midgard-Saga-Niflheim/Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    private Rigidbody2D rgb2d;
-    private Vector3 change;
+    public float speed = 10.0f;
+    private Rigidbody2D rb;
+    public Vector2 movement;
+    //private Vector3 change;
 
     // Start is called before the first frame update
     private void Start()
     {
-        rgb2d = GetComponent<Rigidbody2D>();
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        change = Vector3.zero;
-        change.x = Input.GetAxisRaw("Horizontal");
-        change.y = Input.GetAxisRaw("Vertical");
-        if (change != Vector3.zero)
-        {
-            MoveCharacter();
-        }
+        //change = Vector3.zero;
+        //change.x = Input.GetAxisRaw("Horizontal");
+        //change.y = Input.GetAxisRaw("Vertical");
+        //if (change != Vector3.zero)
+        //{
+        //    MoveCharacter();
+        //}
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
-    private void MoveCharacter()
+    private void FixedUpdate()
     {
-        rgb2d.MovePosition(transform.position + change * speed * Time.fixedDeltaTime);
+        MoveCharacter(movement);
     }
+
+    private void MoveCharacter(Vector2 direrction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direrction * speed * Time.deltaTime));
+    }
+
+    //private void MoveCharacter()
+    //{
+    //    rb.MovePosition(transform.position + change * speed * Time.fixedDeltaTime);
+    //}
 }
